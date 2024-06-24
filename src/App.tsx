9600415413f4efa1a48dfe40eb8cd321013ce72d
Olivier Roche <olivier@studio-gram.com>
2024-06-24 14:44:13 +0200

    Add museum marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
+import "./App.scss";
 import MapMuseum from "./components/map/MapMuseum";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <MapMuseum />
     </div>
   );
-}
+};
 
 export default App;
