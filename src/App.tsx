0a826652a057d4e138f64a374d4367a28f828a67
Olivier Roche <olivier@studio-gram.com>
2024-06-25 09:56:40 +0200

    Add info sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import "./App.scss";
+import { useContext, useEffect } from "react";
+import { StateContext } from "./context/state.context";
+import InfoMuseum from "./components/info/InfoMuseum";
 import MapMuseum from "./components/map/MapMuseum";
 
+import "./App.scss";
+
 const App = () => {
+  const { setIsMobile } = useContext(StateContext);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("resize", checkMobile);
+      window.addEventListener("orientationchange", checkMobile);
+      window.addEventListener("load", checkMobile);
+      window.addEventListener("reload", checkMobile);
+    }
+  }, []);
+
+  const checkMobile = () => {
+    setIsMobile(window.innerWidth > 768 ? false : true);
+  };
+
   return (
-    <div className="App">
+    <main>
       <MapMuseum />
-    </div>
+      <InfoMuseum />
+    </main>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import { MapProvider } from "./context/map.context";
+import { StateProvider } from "./context/state.context";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MapProvider>
-      <App />
-    </MapProvider>
+    <StateProvider>
+      <MapProvider>
+        <App />
+      </MapProvider>
+    </StateProvider>
   </React.StrictMode>
 );
